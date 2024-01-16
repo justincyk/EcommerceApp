8bed1a8599e7712321b80d39e3dcf268193b698c
justincyk <jcykim@csu.fullerton.edu>
2024-01-15 21:21:39 -0800

    Created a shop component and a cart component

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -2,7 +2,6 @@ import { createContext, useState, useEffect } from "react";
 import {
   onAuthStateChangedListener,
   createUserDocumentFromAuth,
-  signOutUser,
 } from "../utils/firebase/firebase.utils";
 
 //  as the actual value you want to access
